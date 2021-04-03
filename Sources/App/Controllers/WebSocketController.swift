5f70a778b1a0e6da0841ac2faf38daa6733e4bbf
Nicopicks <34891827+Nicopicks@users.noreply.github.com>
2021-04-03 10:34:59 +0200

    websocket: sort by descending

--- Sources/App/Controllers/WebSocketController.swift
+++ Sources/App/Controllers/WebSocketController.swift
@@ -28,6 +28,7 @@ class WebSocketController {
         var messagesToSend : [Message.MessageToSend] = []
         _ = Message.query(on: req.db)
             .with(\.$ownerId)
+            .sort(\.$timestamp, .descending)
             .all()
             .map { (messages) in
                 for message in messages {
