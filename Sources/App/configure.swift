2f9374eea3bf3995ab9d75d4da67fb5d8c7eb706
cedric06nice <cedric06nice@mac.com>
2021-04-06 19:58:24 +0100

    UpdateDBPicture

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,24 +8,24 @@ public func configure(_ app: Application) throws {
     
     app.routes.defaultMaxBodySize = "10mb"
     
-//    if app.environment == .production {
-//        app.http.server.configuration.hostname = Secrets.Server.hostname
-//        app.http.server.configuration.port = Secrets.Server.port
-//
-//        app.databases.use(.mysql(hostname: Secrets.MySQL.hostname,
-//                                username: Secrets.MySQL.username,
-//                                 password: Secrets.MySQL.password,
-//                                 database: Secrets.MySQL.database,
-//                                 tlsConfiguration: TLSConfiguration.forClient(certificateVerification: .none)), as: .mysql)
-//    } else {
+    if app.environment == .production {
+        app.http.server.configuration.hostname = Secrets.Server.hostname
+        app.http.server.configuration.port = Secrets.Server.port
+
+        app.databases.use(.mysql(hostname: Secrets.MySQL.hostname,
+                                username: Secrets.MySQL.username,
+                                 password: Secrets.MySQL.password,
+                                 database: Secrets.MySQL.database,
+                                 tlsConfiguration: TLSConfiguration.forClient(certificateVerification: .none)), as: .mysql)
+    } else {
         app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
-//    }
+    }
     
     // configure migrations
     app.migrations.add(CreateUser())
     app.migrations.add(CreateUserToken())
     app.migrations.add(CreateMessage())
-    app.migrations.add(UpdateMessageIsRead())
+    app.migrations.add(DBUpdatePicture())
 
     // enable automatic migration
     try app.autoMigrate().wait()
